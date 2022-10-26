d9da254589766c294732619fd39b3151a4e4a710
Dieronn <116627648+Dieronn@users.noreply.github.com>
2022-10-26 09:25:54 +0200

    ReadWrite

---
+++ ReadWrite/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ReadWrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.txtHeader = new System.Windows.Forms.TextBox();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(27, 27);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(121, 27);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(219, 399);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(425, 27);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(88, 399);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 4;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(521, 27);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Location = new System.Drawing.Point(27, 79);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(42, 13);
+            this.lblHeader.TabIndex = 6;
+            this.lblHeader.Text = "Header";
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(27, 114);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(28, 13);
+            this.lblText.TabIndex = 7;
+            this.lblText.Text = "Text";
+            // 
+            // txtHeader
+            // 
+            this.txtHeader.Location = new System.Drawing.Point(88, 76);
+            this.txtHeader.Name = "txtHeader";
+            this.txtHeader.Size = new System.Drawing.Size(206, 20);
+            this.txtHeader.TabIndex = 8;
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(88, 102);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(206, 284);
+            this.txtText.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(316, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(285, 310);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(320, 27);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(320, 409);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(75, 13);
+            this.lblCount.TabIndex = 12;
+            this.lblCount.Text = "Count o f rows";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 450);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.txtHeader);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.TextBox txtHeader;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ ReadWrite/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadWrite
+{
+    public partial class Form1 : Form
+    {
+        DataTable table1 = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Text files (.txt)|*txt|All files(*.*)|*.*";
+            open.Title = "Open txt";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    StreamReader streamR = new StreamReader(open.FileName);
+                    string line;
+                    string[] values;
+
+                    while (!streamR.EndOfStream)
+                    {
+                        line = streamR.ReadLine();
+                        values = line.Split(',');
+                        string[] row = new string[values.Length];
+
+                        for (int j = 0; j < values.Length; j++)
+                        {
+                            row[j] = values[j].Trim();
+                        }
+
+                        table1.Rows.Add(row);                                              
+                    }
+                    streamR.Close();
+
+                }
+                catch
+                {
+                    MessageBox.Show("Unexpected error while opening");
+                }
+                lblCount.Text = "Count of rows " + dataGridView1.Rows.Count;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Text files (.txt)|*.txt|All files|*.*";
+            save.Title = "Table";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    StreamWriter streamW = new StreamWriter(save.FileName);
+                    for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                        {   
+                            if(j!= dataGridView1.Columns.Count-1)
+                            streamW.Write(dataGridView1.Rows[i].Cells[j].Value.ToString() + ",");
+                            else streamW.Write(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                        }
+                        streamW.WriteLine("");
+                    }
+                    streamW.Close();
+
+                    MessageBox.Show("Saved");
+                }
+                catch
+                {
+                    MessageBox.Show("Unexpected error while saving");
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            table1.Rows.Add(txtHeader.Text, txtText.Text);
+            txtHeader.Clear();
+            txtText.Clear();
+            lblCount.Text = "Count of rows " + dataGridView1.Rows.Count;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            table1.Rows[index].Delete();
+            lblCount.Text = "Count of rows " + dataGridView1.Rows.Count;
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtHeader.Clear();
+            txtText.Clear();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            if (index >= 0)
+            {
+                txtHeader.Text = table1.Rows[index].ItemArray[0].ToString();
+                txtText.Text = table1.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table1.Columns.Add("Header", typeof(string));
+            table1.Columns.Add("Message", typeof(string));
+            dataGridView1.DataSource = table1;
+            dataGridView1.Columns["Header"].Width = 140;
+            dataGridView1.Columns["Message"].Width = 140;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {   
+            while (dataGridView1.Rows.Count > 0)
+            {
+                table1.Rows[0].Delete();
+            }            
+            lblCount.Text = "Count of rows " + dataGridView1.Rows.Count;
+        }
+    }
+}
